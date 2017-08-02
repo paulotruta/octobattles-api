aed465c1e5aa55e3ede594e8cf67e41240538cba
Paulo Truta <jpt@eurotux.com>
2017-08-02 08:46:54 +0100

    Added battles table migration

---
+++ models/migrations/battles.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: 31-Jul-2017 às 23:24
+-- Versão do servidor: 10.1.23-MariaDB
+-- PHP Version: 7.1.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `octobattles`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `battles`
+--
+
+CREATE TABLE IF NOT EXISTS `battles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `character1_id` int(11) NOT NULL,
+  `character2_id` int(11) NOT NULL,
+  `victorious_character_id` int(11) NOT NULL,
+  `battle_log` varchar(10000) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `battle_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `character1_id` (`character1_id`),
+  KEY `character2_id` (`character2_id`),
+  KEY `victorious_character_id` (`victorious_character_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Limitadores para a tabela `battles`
+--
+ALTER TABLE `battles`
+  ADD CONSTRAINT `battles_ibfk_1` FOREIGN KEY (`character1_id`) REFERENCES `characters` (`id`),
+  ADD CONSTRAINT `battles_ibfk_2` FOREIGN KEY (`character2_id`) REFERENCES `characters` (`id`),
+  ADD CONSTRAINT `battles_ibfk_3` FOREIGN KEY (`victorious_character_id`) REFERENCES `characters` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
